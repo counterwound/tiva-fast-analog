1d73ce4888501147e506c34f08969d67d9ee8402
Derek Janak <derek.janak@ssi-us.sevun.com>
2018-04-05 15:22:04 -0500

    This is software sampling from ADC based on pin C5 interrupt

--- main.c
+++ main.c
@@ -29,26 +29,18 @@
 #include "driverlib/interrupt.h"
 #include "driverlib/pin_map.h"
 #include "driverlib/sysctl.h"
-#include "driverlib/timer.h"
-#include "driverlib/uart.h"
-#include "driverlib/udma.h"
 #include "pinmux.h"
 
 #include "utils/uartstdio.h"
 
 #define ADC_SAMPLE_BUF_SIZE 1024
 
-#define UDMA_CHCTL_XFERMODE_M   0x00000007  // uDMA Transfer Mode
 
 //*****************************************************************************
 // Variables
 //*****************************************************************************
 
-uint32_t status = 0;
-
-uint16_t DMAControlTable[1024/sizeof(uint32_t)] __attribute__(( aligned(1024) ));
-
-uint16_t ADCValues2[ADC_SAMPLE_BUF_SIZE];
+uint32_t ADCValues[ADC_SAMPLE_BUF_SIZE];
 //*****************************************************************************
 // Timers Setup
 //*****************************************************************************
@@ -62,7 +54,7 @@ uint16_t ADCValues2[ADC_SAMPLE_BUF_SIZE];
 //*****************************************************************************
 void ConfigureADC(void)
 {
-    ADCSequenceConfigure(ADC0_BASE, 0, ADC_TRIGGER_EXTERNAL, 0);
+    ADCSequenceConfigure(ADC0_BASE, 0, ADC_TRIGGER_PROCESSOR, 0);
     GPIOADCTriggerEnable(GPIO_PORTB_BASE,GPIO_PIN_4);
 
     ADCSequenceStepConfigure(ADC0_BASE, 0, 0, ADC_CTL_CH0);
@@ -72,84 +64,34 @@ void ConfigureADC(void)
     ADCSequenceStepConfigure(ADC0_BASE, 0, 4, ADC_CTL_CH4);
     ADCSequenceStepConfigure(ADC0_BASE, 0, 5, ADC_CTL_CH5);
     ADCSequenceStepConfigure(ADC0_BASE, 0, 6, ADC_CTL_CH6);
-    ADCSequenceStepConfigure(ADC0_BASE, 0, 7, ADC_CTL_CH7 |ADC_CTL_IE | ADC_CTL_END);
+    ADCSequenceStepConfigure(ADC0_BASE, 0, 7, ADC_CTL_CH7 | ADC_CTL_IE | ADC_CTL_END);
 
     ADCSequenceEnable(ADC0_BASE, 0);
     ADCIntClear(ADC0_BASE, 0);
 }
 
-void ADCprocess(uint32_t ch)
-{
-    if ((((tDMAControlTable *) DMAControlTable)[ch].ui32Control & UDMA_CHCTL_XFERMODE_M) != UDMA_MODE_STOP) return;
-   // uDMAChannelModeGet(ch)
-    // store the next buffer in the uDMA transfer descriptor
-    // the ADC is read directly into the correct emacBufTx to be transmitted
-    uDMAChannelTransferSet(ch, UDMA_MODE_PINGPONG, (void *)(ADC0_BASE + ADC_O_SSFIFO0), ADCValues2, ADC_SAMPLE_BUF_SIZE);
-}
-
-void ADC0IntHandler()
-{
-    // this interrupt handler is optimized to use as few CPU cycles as possible
-    // it must handle ADC_SAMPLE_BUF_SIZE samples in 1/125000 seconds
-    //
-    // optimizations include:
-    // 1. choosing ADC_SAMPLE_BUF_SIZE to minimize overhead
-    // 2. replacing calls to ROM_* functions with raw memory accesses (less portable)
-
-    status = ADCIntStatus(ADC0_BASE, 0, true);
-    ADCIntClear(ADC0_BASE, status);
-    *(uint32_t *) (ADC0_BASE + ADC_O_ISC) = ADC_INT_DMA_SS0;    // optimized form of ROM_ADCIntClearEx(ADC0_BASE, ADC_INT_DMA_SS0);
-
-    //ADCprocess(UDMA_CHANNEL_ADC0 | UDMA_PRI_SELECT);
-    //ADCprocess(UDMA_CHANNEL_ADC0 | UDMA_ALT_SELECT);
-}
-
 //*******************************************************************************
 // uDMA Setup
 //*****************************************************************************
-void ConfigureUDMA(void){
-
-    // Enable the UDMA peripheral
-    //
-//    SysCtlPeripheralEnable(SYSCTL_PERIPH_UDMA);
-    //
-    // Wait for the UDMA module to be ready.
-    //
-//    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_UDMA))
-    {
-    }
-    //
-    // Enable the uDMA controller.
-    //
-//    uDMAEnable();
-    //
-    // Set the base for the channel control table.
-    //
-//    uDMAControlBaseSet(&DMAControlTable[0]);
-//    ADCSequenceDMAEnable(ADC0_BASE, 0);
-
-//    uDMAChannelAttributeDisable(UDMA_CHANNEL_ADC0, UDMA_ATTR_ALTSELECT /*start with ping-pong PRI side*/ |
-//            UDMA_ATTR_HIGH_PRIORITY /*low priority*/ | UDMA_ATTR_REQMASK /*unmask*/);
-        // enable some bits
-//    uDMAChannelAttributeEnable(UDMA_CHANNEL_ADC0, UDMA_ATTR_USEBURST /*only allow burst transfers*/);
-
-//    uDMAChannelControlSet(UDMA_CHANNEL_ADC0 | UDMA_PRI_SELECT, UDMA_SIZE_16 | UDMA_SRC_INC_NONE | UDMA_DST_INC_16 | UDMA_ARB_1024);
-//    uDMAChannelControlSet(UDMA_CHANNEL_ADC0 | UDMA_ALT_SELECT, UDMA_SIZE_16 | UDMA_SRC_INC_NONE | UDMA_DST_INC_16 | UDMA_ARB_1024);
-    //uDMAChannelTransferSet(UDMA_CHANNEL_ADC0 | UDMA_PRI_SELECT, UDMA_MODE_PINGPONG, (void *)(ADC0_BASE + ADC_O_SSFIFO0), ADCValues2, ADC_SAMPLE_BUF_SIZE);
-    //uDMAChannelTransferSet(UDMA_CHANNEL_ADC0 | UDMA_ALT_SELECT, UDMA_MODE_PINGPONG, (void *)(ADC0_BASE + ADC_O_SSFIFO0), ADCValues2, ADC_SAMPLE_BUF_SIZE);
-    //IntEnable(INT_ADC0SS0);
-
-    //ADCIntEnableEx(ADC0_BASE, ADC_INT_DMA_SS0);
-//    uDMAChannelEnable(UDMA_CHANNEL_ADC0);
-
-}
-
-
 
 //*****************************************************************************
 // GPIO Setup
 //*****************************************************************************
 
+volatile bool g_bEventGPIOFlag = 0;         // External Event input
+
+// Immediately trigger a message burst if the External fault occurs
+void PortCIntHandler(void)
+{
+    uint32_t ui32StatusGPIOC;
+    ui32StatusGPIOC = GPIOIntStatus(GPIO_PORTC_BASE,true);
+    GPIOIntClear(GPIO_PORTC_BASE, ui32StatusGPIOC);
+
+    if ( GPIO_INT_PIN_5 == (ui32StatusGPIOC & GPIO_INT_PIN_5) )
+    {
+        g_bEventGPIOFlag = 1;
+    }
+}
 //*****************************************************************************
 // Configure the interrupts
 //*****************************************************************************
@@ -158,12 +100,11 @@ void ConfigureInterrupts(void)
     // Enable processor interrupts.
     IntMasterEnable();
 
-    GPIOPadConfigSet(GPIO_PORTB_BASE,GPIO_PIN_4,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPD);
-    GPIOIntTypeSet(GPIO_PORTB_BASE,GPIO_PIN_4,GPIO_FALLING_EDGE);
-    //GPIOADCTriggerEnable(GPIO_PORTF_BASE, GPIO_PIN_4);
+    GPIOPadConfigSet(GPIO_PORTC_BASE,GPIO_PIN_5,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD);
+    GPIOIntTypeSet(GPIO_PORTC_BASE,GPIO_PIN_5,GPIO_FALLING_EDGE);
+    GPIOIntEnable(GPIO_PORTC_BASE, GPIO_PIN_5);
 
-    ADCIntEnable(ADC0_BASE, 0);
-    ADCIntRegister(ADC0_BASE,0,ADC0IntHandler);
+    GPIOIntRegister(GPIO_PORTC_BASE,PortCIntHandler);
 
 }
 
@@ -182,7 +123,6 @@ int main(void)
 
     // Initialize and setup the ports
     PortFunctionInit();
-    ConfigureUDMA();
     ConfigureADC();
     ConfigureInterrupts();
 
@@ -196,6 +136,16 @@ int main(void)
         //*****************************************************************************
         // Event
         //*****************************************************************************
-        status = ADCIntStatus(ADC0_BASE, 0, true);
+
+        if(g_bEventGPIOFlag){
+            g_bEventGPIOFlag = 0;
+            int i = 0;
+            for(i = 0; i < 1024; i += 8){
+                ADCProcessorTrigger(ADC0_BASE, 0);
+                while(!ADCIntStatus(ADC0_BASE, 0, false)) {}
+                ADCIntClear(ADC0_BASE, 0);
+                ADCSequenceDataGet(ADC0_BASE, 0, &ADCValues[i]);
+            }
+        }
     }
 }
